c3aa70a58bd66a64177b9903f689f4cba4d48383
Mosi Jones <jon0020@northcote-hs.edu.vic.gov.au>
2018-05-03 12:22:34 +1000

    Made class roll

---
+++ classroll.py
@@ -0,0 +1,12 @@
+roll = ['Jessica', 'Emily', 'Jordan', 'Kayley', 'Bruce', 'Michael', 'Everett', 'Lisa', 'Sam', 'Noah']
+
+student1 = roll[2]
+print(student1)
+
+enrollment = len(roll)
+
+roll.append("James")
+
+del roll[2]
+
+roll[5]= "Mike"
