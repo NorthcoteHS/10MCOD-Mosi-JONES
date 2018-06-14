b08a5d133f094978cf4a9f5b62abd93035a7baa0
Mosi Jones <jon0020@northcote-hs.edu.vic.gov.au>
2018-06-14 12:49:37 +1000

    boredgame CAT

---
+++ boredgame.py
@@ -0,0 +1,11 @@
+print: "Welcome to the Bored Game! This will give you ideas on what to do if you're bored."
+list(string.ascii_lowercase)
+['a', 'b', 'c', 'd'
+
+'a'=="Walk the dog"
+'b'== "Go for a walk"
+'c'== "Make some food"
+'d'== "d"
+
+ print: "Why dont you try...() "
+
---
+++ countdown.py
@@ -0,0 +1,5 @@
+import time
+
+for counter in range(0,11):
+    print(counter)
+    time.sleep (1)
